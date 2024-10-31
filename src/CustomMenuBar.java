290d2b1215a27d3efb304e2107c0fdfd8dfc1af1
E-xml <155318575+E-xml@users.noreply.github.com>
2024-10-31 01:56:24 +0100

    Update CustomMenuBar.java

--- src/CustomMenuBar.java
+++ src/CustomMenuBar.java
@@ -7,7 +7,7 @@ public class CustomMenuBar extends MenuBar {
         Menu filemenu = new Menu("File");
         MenuComponent[] filemenuitems = {new MenuItem("New"), new MenuItem("New window"), new MenuSpace(),new MenuItem("Open"), new MenuItem("Save"), new MenuItem("Save as"), new MenuItem("Print"), new MenuItem("Close"), new MenuItem("Close Window"), new MenuSpace(), new MenuItem("Exit")};
         Menu editmenu = new Menu("Edit");
-        MenuComponent[] editmenuitems = {new MenuItem("Undo"), new MenuItem("Redo"), new MenuSpace()
+        MenuComponent[] editmenuitems = {new MenuItem("Undo"), new MenuItem("Redo"), new MenuSpace(), new MenuItem("Copy"), new MenuItem("Cut"), new MenuItem("Paste"), new MenuSpace(), new MenuItem("Delete")};
         Menu selectionmenu = new Menu("Selection");
         Menu appearancemenu = new Menu("Appearance");
         Menu securitymenu = new Menu("Security");
