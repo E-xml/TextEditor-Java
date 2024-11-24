eb38017fb1e36b335811069bc2f0222f9bddd4ae
E-xml <155318575+E-xml@users.noreply.github.com>
2024-11-24 16:01:32 +0100

    Update CustomMenuBar.java

--- src/CustomMenuBar.java
+++ src/CustomMenuBar.java
@@ -2,9 +2,7 @@ package src;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.Objects;
 
 public class CustomMenuBar extends MenuBar {
